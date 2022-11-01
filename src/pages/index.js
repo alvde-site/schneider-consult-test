20f1b9a25d9fe57baf47e82c714672d4f0ff4972
Aleksandr Demidenko <sashatro@mail.ru>
2022-11-02 01:36:15 +0300

    feat: Add getting next monday

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,14 @@
 import './index.css';
+
+const promoDate = document.querySelector('.header__promo-date');
+
+const date = new Date();
+const DayToNextMonday = date.setDate(date.getDate() + (7 - date.getDate()) % 7);
+const nextMonday = new Date(DayToNextMonday);
+const newDate = `${nextMonday.getDate()}.${nextMonday.getMonth()}.${nextMonday.getFullYear() - 2000}`;
+console.log(newDate)
+
+
+
+
+promoDate.textContent = newDate;
